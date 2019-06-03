27993b21427be1c8519a5aecbf63ded78c7e47e0
Sunny Dosanjh <policynumber@hotmail.com>
2019-06-03 22:46:25 +0100

    More changes

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,18 +3,23 @@ import React, { Component } from 'react'
 class QuizQuestion extends Component
 {
 
-    render(){return  (<main>
-        <section>
-            <p>{this.props.quiz_question.intruction_text}</p>
-        </section>
-        <section className="buttons">
-            <ul>
-                <li>
-                    {this.props.quiz_question.answer_options[0]}
-                </li>
-            </ul>
-        </section>
-    </main>)}   
+    render()
+    {  
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <li>
+                            {this.props.quiz_question.answer_options[0]}
+                        </li>
+                    </ul>
+                </section>
+            </main>
+        )
+    }   
 }
 
 
