bf1b744b384b67d91d3990edfe6f0c74f1aaaf5e
Sunny Dosanjh <sunny.dosanjh@ridgeway.com>
2019-06-08 20:08:14 +0100

    Error message based on state.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,18 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component
 {
+    constructor(props) {
+        super(props)
+        this.state = { incorrectAnswer: false }
+    }
+    
     handleClick(buttonText) {
-        if(buttonText === this.props.quiz_question.answer) {
+        if( buttonText === this.props.quiz_question.answer ) {
+            this.setState({ incorrectAnswer: false } )
             this.props.showNextQuestionHandler()
+        }
+        else {
+            this.setState( {incorrectAnswer: true })
         }
     }
 
@@ -25,6 +34,7 @@ class QuizQuestion extends Component
                         
                     </ul>
                 </section>
+                { this.state.incorrectAnswer ? <p className="error">Sorry that's not right</p> : null }
             </main>
         )
     }   
