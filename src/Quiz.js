464e964f2437f9859f19fe55cd9c28053ddb981d
Sunny Dosanjh <sunny.dosanjh@ridgeway.com>
2019-06-08 14:02:04 +0100

    Added QuizEnd component. Added semi colon but not sure why it's needed.

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component{
         this.state = { quiz_position: 1 }
     }
     render(){
-        const isQuizEnd = ((this.quiz_position - 1) === quizData.quiz_questions.length)
+        const isQuizEnd = ((this.quiz_position - 1) === quizData.quiz_questions.length);
         return (
             <div>
                 {isQuizEnd ? <QuizEnd/> :  <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
