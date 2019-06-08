2b197aa63df16b17afdf33730caed36fc0809ca8
Sunny Dosanjh <sunny.dosanjh@ridgeway.com>
2019-06-08 19:53:45 +0100

    Updated bits and added events.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,11 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component
 {
+    handleClick(buttonText) {
+        if(buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler()
+        }
+    }
 
     render()
     {  
@@ -13,7 +18,11 @@ class QuizQuestion extends Component
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                            return <QuizQuestionButton button_text={answer_option} key={index}
+                            clickHandler={this.handleClick.bind(this)}/>
+                        })}
+                        
                     </ul>
                 </section>
             </main>
